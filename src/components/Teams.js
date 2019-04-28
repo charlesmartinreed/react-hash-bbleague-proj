9da415fe04deacde7edc571d73c2490b66615737
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-28 17:05:00 -0500

    Add additional styling to Teams component.

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -42,6 +42,13 @@ export default class Teams extends Component {
 						<Team id={match.params.teamId}>
 							{(team) => team === null ? <h1>LOADING</h1> : <div style={{width: '100%'}}>
 								<TeamLogo id={team.id} className="center" />
+								<h1 className="medium-header">{team.name}</h1>
+								<ul className="info-list row">
+									<li>Established<div>{team.established}</div></li>
+									<li>Manager<div>{team.manager}</div></li>
+									<li>Coach<div>{team.coach}</div></li>
+								</ul>
+									<Link className="center btn-main" to={`/${match.params.teamId}`}>{team.name} Team Page</Link>
 								</div>}
 						</Team>
 					</div>
