a9329dd5954de556fcf5bca9883f9885e62539b2
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-28 16:27:08 -0500

    Render team names in Teams component.

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import { getTeamNames } from '../api';
 
-export default class Players extends Component {
+import { parse } from 'query-string';
+import slug from 'slug';
+
+export default class Teams extends Component {
+	state = {
+		teamNames: [],
+		loading: true,
+	}
+
+	componentDidMount() {
+		getTeamNames()
+			.then((teamNames) => {
+				this.setState(() => ({
+					teamNames,
+					loading: false
+				}))
+			})
+	}
+
 	render() {
+		const { teamNames, loading } = this.state;
+		const { location, match } = this.props;
+
 		return (
 			<div className="container two-column">
-				Teams
+				<Sidebar
+					loading={loading}
+					title='Teams'
+					list={teamNames}
+					{...this.props}
+				/>
+
+				{loading === false && location.pathname === '/teams' ? <div className='sidebar-instruction'>Select a Team</div> : null}
+
 			</div>
 		)
 	}
