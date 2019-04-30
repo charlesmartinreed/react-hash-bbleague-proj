74bd77eb17f10ff9fd1081f81d4282831734e60a
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-30 18:49:02 -0500

    Demonstrate basics of using CSSTransition and TransitionGroup from 'react-transition-group'.

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,65 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Switch, Route, Redirect } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+/* Because we're animating our routes, we need to wrap that code in the TransitionGroup component. Now the component will keep track of the children and their changes, whether they're coming or going, etc. */
+/*
+	CSSTransition props:
+	TIMEOUT is how long TransitionGroup will display all children before removing old children
+
+	classNames is added to the component during prop change diffing
+
+*/
+
+/* by wrapping everything in a Route, we can get access to props. And beause we didn't give it a path, it will always render. */
+
+/* Switch takes an optional location prop, we use that second location to let React know that we're transitioning. Otherwise, the route would neer know that there's a different location since we're using the passed in by the React Context API in our Route component */
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
-				<div style={styles.fill}>
-				<Route exact path='/' render={() => (
-					<Redirect to='/hsl/10/90/50' />
-				)} />
+				<Route render={({location}) => (
+					<div style={styles.fill}>
+					<Route exact path='/' render={() => (
+						<Redirect to='/hsl/10/90/50' />
+					)} />
 
-				<ul style={styles.nav}>
-					<NavLink to="/hsl/10/90/50">Red</NavLink>
-					<NavLink to="/hsl/120/100/40">Green</NavLink>
-					<NavLink to="/rgb/33/150/243">Blue</NavLink>
-					<NavLink to="/rgb/240/90/140">Pink</NavLink>
-				</ul>
+					<ul style={styles.nav}>
+						<NavLink to="/hsl/10/90/50">Red</NavLink>
+						<NavLink to="/hsl/120/100/40">Green</NavLink>
+						<NavLink to="/rgb/33/150/243">Blue</NavLink>
+						<NavLink to="/rgb/240/90/140">Pink</NavLink>
+					</ul>
 
-				<div style={styles.content}>
-					<Switch>
-						<Route exact path='/hsl/:h/:s/:l' component={HSL} />
-						<Route exact path='/rgb/:r/:g/:b' component={RGB} />
-						<Route render={() => <div>Not Found</div>} />
-					</Switch>
-
+					<div style={styles.content}>
+						<TransitionGroup>
+							<CSSTransition
+								key={location.key}
+								timeout={300}
+								classNames='fade'>
+									<Switch location={location}>
+										<Route exact path='/hsl/:h/:s/:l' component={HSL} />
+										<Route exact path='/rgb/:r/:g/:b' component={RGB} />
+										<Route render={() => <div>Not Found</div>} />
+									</Switch>
+							</CSSTransition>
+						</TransitionGroup>
+					</div>
 				</div>
-			</div>
+				)} />
 			</Router>
 		)
 	}
 }
+
+/*
+	TransitionGroup is like a wrapper component - a state machine for managing the mounting and unmounting of component. It's local state keeps track of all of its children. Whenever the props change, it loops over the children and compares the old children to the new children. Once it figures this out, it maps over the children and updates state for them with setState, which causes a re-render. Why does it render old and new children? Because of animations. The old child might need an animation OUT, a new child might need an animation IN.
+*/
+
+/*
+	Takes all info from TransitionGroup and applies classNames to them during appear, exit stages of their transitions. You can use CSS selectors to then customize those classes, as usual.
+*/
 
 const NavLink = (props) => (
 	<li style={styles.navItem}>
